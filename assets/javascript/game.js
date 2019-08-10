86feb245729346b6bf0fcc166db24591c1f82d6b
sjconst <stephanie.c.lake@gmail.com>
2019-08-10 14:48:12 -0700

    base attack power feature added, counter attack display added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -37,45 +37,136 @@
 
 $(document).ready(function() {
 
+var healthPoints, attackPower, counterPoints; 
+var incrementor = [];
+ 
 var DOM = {
-    $img: $(".choice"),
+    $choice: $(".choice"),
+    $imgCont: $(".image-container"),
     $popup: $("#popup"),
-    $appleHP: $(".apple-HP"),
-    $appleAP: $(".apple-AP"),
-    $appleCAP: $(".apple-CAP")
+    $ready: $("#ready")
 };
 
-function init() {
-    gamePlaying = false;
-}
-init();
+var DOMapple = {
+    $HP: $("#apple-HP"),
+    $AP: $("#apple-AP"),
+    $CAP: $("#apple-CAP"),
+};
 
-$(".image-container").on("click", function(){
+var DOMwater = {
+    $HP: $("#water-HP"),
+    $AP: $("#water-AP"),
+    $CAP: $("#water-CAP"),
+};
+
+var DOMgrape = {
+    $HP: $("#grape-HP"),
+    $AP: $("#grape-AP"),
+    $CAP: $("#grape-CAP"),
+};
+
+var DOMlemon = {
+    $HP: $("#lemon-HP"),
+    $AP: $("#lemon-AP"),
+    $CAP: $("#lemon-CAP"),
+};
+
+var setup = {
+    init: function() {
+            gamePlaying = false;
+            appleHP = this.getRandomHP();
+            appleAP = this.getRandomAP();
+            waterHP = this.getRandomHP();
+            waterAP = this.getRandomAP();
+            grapeHP = this.getRandomHP();
+            grapeAP = this.getRandomAP();
+            lemonHP = this.getRandomHP();
+            lemonAP = this.getRandomAP();
+            DOMapple.$HP.text(appleHP); 
+            DOMapple.$AP.text(appleAP); 
+            DOMwater.$HP.text(waterHP); 
+            DOMwater.$AP.text(waterAP);  
+            DOMgrape.$HP.text(grapeHP);  
+            DOMgrape.$AP.text(grapeAP); 
+            DOMlemon.$HP.text(lemonHP);   
+            DOMlemon.$AP.text(lemonAP);          
+            //add popup that bounces up and down until user makes selection
+            },
+    getRandomHP: function() {
+                min = Math.ceil(5);
+                max = Math.floor(20);
+                return Math.floor(Math.random() * (max - min + 1)) + min; 
+                },
+    getRandomAP: function() {
+                min = Math.ceil(1);
+                max = Math.floor(5);
+                return Math.floor(Math.random() * (max - min + 1)) + min; 
+                },
+}
+
+setup.init();
+
+  
+DOM.$imgCont.on("click", function(){
     if($(this).hasClass("choice")){
-        $(".image-container").removeClass("choice");
+        DOM.$imgCont.removeClass("choice");       
+        $("#attackZone").css("background-color", "rgba(255, 255, 255, 0.5)");
         $(this).appendTo("#left-well");
         $(this).addClass("activePlayer");
-        DOM.$img.not(this).addClass("enemy");
-        DOM.$popup.css({"visibility": "visible"}); 
+        DOM.$imgCont.not(this).addClass("enemy");
+        DOM.$popup.css({"visibility": "visible"});   
     }
 });
     
 //close popup, //on click of img with enemy class, that img gets enemy-chosen class, alert "ready to attack!"
-$(".image-container").on("click", function(){
-    if($(this).hasClass("enemy")){
+DOM.$imgCont.on("click", function(){
+    if($(this).hasClass("enemy")){        
         DOM.$popup.css({"visibility": "hidden"});
         $(this).appendTo("#right-well");
         $(this).addClass("enemy-chosen");
+        var APtext = $(".enemy-chosen").find(".AP-text");
+        APtext.text("counter attack power");
         $(this).removeClass("enemy");
         $(".enemy").appendTo("#restZone")
-        $("#ready").css({"visibility": "visible"}); 
+        DOM.$ready.css({"visibility": "visible"}); 
         gamePlaying = true;        
     } 
 });
 
+// * Whenever the player clicks `attack`, their character damages the defender. The opponent will lose `HP` (health points). 
 $(".btn-new").on("click", function(){
     if (gamePlaying) {
-        $("#ready").css({"visibility": "hidden"})
+        DOM.$ready.css({"visibility": "hidden"});
+        var enemyHP = $(".enemy-chosen").find(".HP");
+        var enemyAP = $(".enemy-chosen").find(".AP");
+        var playerHP = $(".activePlayer").find(".HP");
+        var playerAP = $(".activePlayer").find(".AP"); 
+        var enemyHPCont =  enemyHP.text();
+        var enemyAPCont = enemyAP.text(); 
+        var playerHPCont = playerHP.text();
+        var playerAPCont = playerAP.text();           
+
+        if (enemyHPCont >= 0 && playerHPCont >= 0) {
+            //enemy-chosen HP loses points
+            enemyHP.text(enemyHPCont - playerAPCont);
+            //counterattack popup
+            //player loses HP
+            playerHP.text(playerHPCont - enemyAPCont);
+            //player AP increases by base
+            incrementor.push(playerAPCont);
+            console.log(incrementor);
+            playerAP.text(+playerAPCont + +incrementor[0]);
+
+        } else {
+            console.log("else");
+        }
+      
+
+        //   * The opponent character will instantly counter the attack. When that happens, the player's character will lose some of their `HP`. These points are shown at the bottom of the player character's picture.
+        
+        // 3. The player will keep hitting the attack button in an effort to defeat their opponent.
+        
+        // * When the defender's `HP` is reduced to zero or below, remove the enemy from the `defender area`. The player character can now choose a new opponent.
     }
 })
 
