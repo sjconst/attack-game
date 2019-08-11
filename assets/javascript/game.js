16fa867671e4a2b4b653f83a5d18cb4cdfaa1204
sjconst <stephanie.c.lake@gmail.com>
2019-08-11 15:08:04 -0700

    all basic game functionalities fixed, need to fix bug after last enemy chosen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -37,9 +37,8 @@
 
 $(document).ready(function() {
 
-var healthPoints, attackPower, counterPoints; 
-var incrementor = [];
- 
+var healthPoints, attackPower, counterPoints, incrementor; 
+
 var DOM = {
     $choice: $(".choice"),
     $imgCont: $(".image-container"),
@@ -89,7 +88,9 @@ var setup = {
             DOMgrape.$HP.text(grapeHP);  
             DOMgrape.$AP.text(grapeAP); 
             DOMlemon.$HP.text(lemonHP);   
-            DOMlemon.$AP.text(lemonAP);          
+            DOMlemon.$AP.text(lemonAP);  
+            incrementor = [];  
+            DOM.$imgCont.removeClass("activePlayer").removeClass("enemy-chosen").removeClass("enemy").addClass("choice").appendTo("#allFruits");
             //add popup that bounces up and down until user makes selection
             },
     getRandomHP: function() {
@@ -103,9 +104,6 @@ var setup = {
                 return Math.floor(Math.random() * (max - min + 1)) + min; 
                 },
 }
-
-setup.init();
-
   
 DOM.$imgCont.on("click", function(){
     if($(this).hasClass("choice")){
@@ -120,14 +118,13 @@ DOM.$imgCont.on("click", function(){
     
 //close popup, //on click of img with enemy class, that img gets enemy-chosen class, alert "ready to attack!"
 DOM.$imgCont.on("click", function(){
-    if($(this).hasClass("enemy")){        
+    if(!gamePlaying && $(this).hasClass("enemy")){        
         DOM.$popup.css({"visibility": "hidden"});
         $(this).appendTo("#right-well");
         $(this).addClass("enemy-chosen");
         var APtext = $(".enemy-chosen").find(".AP-text");
         APtext.text("counter attack power");
-        $(this).removeClass("enemy");
-        $(".enemy").appendTo("#restZone")
+        $(this).removeClass("enemy");        
         DOM.$ready.css({"visibility": "visible"}); 
         gamePlaying = true;        
     } 
@@ -144,7 +141,8 @@ $(".btn-new").on("click", function(){
         var enemyHPCont =  enemyHP.text();
         var enemyAPCont = enemyAP.text(); 
         var playerHPCont = playerHP.text();
-        var playerAPCont = playerAP.text();           
+        var playerAPCont = playerAP.text(); 
+        var $enemyChosen = $(".enemy-chosen");          
 
         if (enemyHPCont >= 0 && playerHPCont >= 0) {
             //enemy-chosen HP loses points
@@ -157,27 +155,18 @@ $(".btn-new").on("click", function(){
             console.log(incrementor);
             playerAP.text(+playerAPCont + +incrementor[0]);
 
+        } else if(playerHPCont <= 0) {
+            alert("You lost!"); //change to popup
+            setup.init();
         } else {
-            console.log("else");
+            $enemyChosen.appendTo("#restZone");
+            $enemyChosen.removeClass("enemy-chosen");
+            DOM.$popup.css({"visibility": "visible"});  
+            gamePlaying = false;
         }
-      
-
-        //   * The opponent character will instantly counter the attack. When that happens, the player's character will lose some of their `HP`. These points are shown at the bottom of the player character's picture.
-        
-        // 3. The player will keep hitting the attack button in an effort to defeat their opponent.
-        
-        // * When the defender's `HP` is reduced to zero or below, remove the enemy from the `defender area`. The player character can now choose a new opponent.
     }
 })
 
-
-
-
-
-
-
-
-
-
+setup.init();
 
   });
